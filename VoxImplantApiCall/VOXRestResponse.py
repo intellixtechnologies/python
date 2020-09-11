2521c26a0393e6cf4babe82a6176367742b826af
Abhishek Roy <abhishek2002@gmail.com>
2020-09-11 17:56:41 +0530

    sms delivery related changes

--- VoxImplantApiCall/VOXRestResponse.py
+++ VoxImplantApiCall/VOXRestResponse.py
@@ -1,4 +1,3 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 from flask import Flask,jsonify,request
 
 app = Flask(__name__)
@@ -20,12 +19,12 @@ def callstatus():
         phone = reqdata['phone']
         status = reqdata['broadcaststatus']
         params = reqdata['params']
-        
-        print("fname: {}\n lname: {} \n phone: {}\n braodcaststatus: {}\n params:{}".format(fname,lname,phone,status,params))
+        smsdelivered = reqdata['smsdelivered']
+        print("fname: {}\n lname: {} \n phone: {}\n braodcaststatus: {}\n smsdelivered: {}\n params:{}".format(fname,lname,phone,status,smsdelivered,params))
     except:
         raise
-    #print(jsonify({"status":"call successful"}))
-    return (jsonify({"status":200}))
+    
+    return (jsonify({"status":200, "fname":reqdata['fname'], "lname":reqdata['lname'],"phone":reqdata['phone'],"status":reqdata['broadcaststatus'],"smsdelivered":reqdata['smsdelivered'],"params":reqdata['params']}))
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5555)
 
--- VoxImplantApiCall/VoxAPICall.py
+++ VoxImplantApiCall/VoxAPICall.py
@@ -3,12 +3,17 @@ import json
 endpoint_url="https://kit.voximplant.com/api/v3/scenario/runScenario" #RunSCenario
 
 request_parameters =  dict()
-fname = "Abhishek" #customer fname
-lname = "Roy" #customer lname
+fname = "John" #customer fname
+lname = "Rambo" #customer lname
+# Max variable/param length for vox is 255. 
+# So need to split between custommsg 1 and 2 if more than 255. Else send a " " for custommsg1. Don't send a Null.
+custommsg = "With the Flu season just round the corner, Cardinal Health is happy to announce that we are ready with your Flu shots. You can now pre-order the flu shots and book an appointment for your entire family from our website or by visiting your nearest pharmacy."
+custommsg1= "Our customer executives will be happy to assist you with more details on the overall booking process, offers and pricing. Would you like to hear back from one of our representatives? You may say Yes to schedule a call or No to disconnect."
+smstext = "Cardinal Health is ready with your Flu shots. We will call you soon with more details."
 request_parameters["phone"]="918017777750" #customer phone number
 request_parameters["phone_number_id"]="1853"
 request_parameters["scenario_id"]="15682"
-request_parameters["variables"]=json.dumps({"fname":fname,"lname":lname,"broadcaststatus":"N"})
+request_parameters["variables"]=json.dumps({"fname":fname,"lname":lname,"broadcaststatus":"N","custommsg":custommsg,"custommsg1":custommsg1,"smstext":smstext,"smsdelivered":"N"})
 request_parameters["domain"]="tcs"
 request_parameters["access_token"]="79b33f8548b585bb93c51e3af9adbfa1"
 
